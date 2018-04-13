d66c87634cf81f7fbc4ff94f435e43a112b9f8b1
Jocke <persson.fristedt@comhem.se>
2018-04-13 14:13:59 +0200

    the end

---
+++ react-flashcards/src/Config/Firebase/db_config.js
@@ -0,0 +1,8 @@
+export const DB_CONFIG = {
+    apiKey: "AIzaSyA2jHIFHHDtcKPL4BsQcOt-iFaRGZ4kd-Y",
+    authDomain: "react-flashcards-5b739.firebaseapp.com",
+    databaseURL: "https://react-flashcards-5b739.firebaseio.com",
+    projectId: "react-flashcards-5b739",
+    storageBucket: "",
+    messagingSenderId: "591858167559"
+}
